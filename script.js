ca4c03daf81ac257b0bc3ea0efdf5cc1d2eb7854
Vidar Franzén <vexxart@gmail.com>
2019-05-09 00:12:39 +0200

    managed to store a form into variable, now to use it.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const mainDiv = document.getElementById("mainDiv");
-var long, lat;
-var local = document.createElement('div')
 const hpKey = "4ac1f1eb94344e009c98b323a4f4a2af";
 const slKey = "2fddd8be222741dca106f37a60e159f7";
-var hpsearch = document.getElementById("hplats").value;
-var hpurl = "api.sl.se/api2/nearbystopsv2.json?key="+ hpKey +"&searchstring=" + hpsearch +"&stationsonly=true&maxresults=1";
+var hpurl;
 
-console.log(hpurl);
+function setUrl()
+{
+    var g_station = document.getElementById('station').value;
+    hpurl = "api.sl.se/api2/nearbystopsv2.json?key="+ hpKey +"&searchstring=" + g_station +"&stationsonly=true&maxresults=1";
+    console.log(hpurl);
+}
