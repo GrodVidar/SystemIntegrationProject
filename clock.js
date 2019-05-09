8b575a460d7fcae79918900c9f66ba802488ddb7
GrodVidar <vexxart@gmail.com>
2019-05-09 10:52:54 +0200

    Added js-file to show current date and clock time.

---
+++ clock.js
@@ -0,0 +1,19 @@
+const clockPara = document.getElementById('clockDiv');
+
+const clockurl = "https://cors-anywhere.herokuapp.com/http://worldtimeapi.org/api/timezone/Europe/Stockholm"
+
+fetch(clockurl)
+    .then(cResp => cResp.json())
+        .then(cData =>
+        {
+            var fullString = cData.datetime;
+            //console.log(fullString);
+            var tPos = fullString.indexOf("T");
+            var dotPos = fullString.indexOf(".");
+            //console.log(tPos);
+            var currentDate = fullString.slice(0,tPos);
+            var currentClock = fullString.slice((tPos+1),dotPos);
+
+            clockPara.innerHTML = `Date: ${currentDate} <br>`;
+            clockPara.innerHTML += `Clock ${currentClock}`;
+        })
